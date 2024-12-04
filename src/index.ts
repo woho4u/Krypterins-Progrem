ed8d903ed02ae17d572e4a15a174bf2fb4a9a26e
Oliver Eriksen <okebedrift@gmail.com>
2024-12-04 09:10:44 +0100

    Initialize project with TypeScript setup, encryption functions, and configuration files

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+//hva slags algorytme er "aes-256-cbc"? er det andre AES algorytmer?
+//hva slags nøkkel er det som blir generert? er det en symmetrisk nøkkel?
+//hva er salt? hvorfor er det brukt her?
+//hva er iv? hvorfor er det brukt her?
+//hva er scryptSync? hva gjør den?
+//hva er createCipheriv? hva gjør den?
+//hva er createDecipheriv? hva gjør den?
+
+import { createCipheriv, createDecipheriv, randomBytes, scryptSync } from "crypto";
+import { readFileSync } from "fs";
+import * as readline from "readline";
+
+const algorithm = "aes-256-cbc";
+
+const iv = randomBytes(16);
+
+export function encrypt(text: string, key: Buffer): string {
+   const cipher = createCipheriv(algorithm, key, iv);
+   let encrypted = cipher.update(text, "utf8", "hex");
+   encrypted += cipher.final("hex");
+   return encrypted;
+}
+
+export function decrypt(encryptedText: string, key: Buffer): string {
+   const decipher = createDecipheriv(algorithm, key, iv);
+   let decrypted = decipher.update(encryptedText, "hex", "utf8");
+   decrypted += decipher.final("utf8");
+   return decrypted;
+}
+
+const originalText = readFileSync("text.txt", "utf8");
+
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout,
+});
+function askUser() {
+   rl.question(
+      "1: Generate a random encryption key.   2: Write your own encryption key: ",
+      (userInput) => {
+         if (userInput === "1") {
+            const key = scryptSync(randomBytes(32), "saltVerdi", 32);
+            runAndLog(key);
+         } else if (userInput === "2") {
+            rl.question("Enter your encryption key: ", (userInput) => {
+               const key = scryptSync(userInput, "saltVerdi", 32);
+               runAndLog(key);
+            });
+         } else {
+            console.log("Invalid input. Please try again.");
+         }
+      }
+   );
+}
+askUser();
+
+
+export function runAndLog(key: Buffer) {
+   const encryptedText = encrypt(originalText, key);
+   const decryptedText = decrypt(encryptedText, key);
+   console.log(`Key: ${key.toString("hex")}`);
+   console.log(`Original Text: ${originalText}`);
+   console.log(`Encrypted Text: ${encryptedText}`);
+   console.log(`Decrypted Text: ${decryptedText}`);
+}
